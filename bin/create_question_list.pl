b2dcfd126c03426eb2649fa1aa02cc9bc1a2ca2f
Leo Lapworth <leo@cuckoo.org>
2012-01-02 11:13:49 +0000

    Simplify auto generation of perlfaq.pod (move logic into the script)

--- bin/create_question_list.pl
+++ bin/create_question_list.pl
@@ -15,6 +15,7 @@ use warnings;
 
 use Path::Class;
 use Data::Dumper;
+use Template;
 use HTML::TreeBuilder;
 use Pod::Simple::XHTML;
 
@@ -22,72 +23,88 @@ use Pod::Simple::XHTML;
 my $SOURCE       = './lib';
 my $HTML_CHARSET = 'UTF-8';
 
-{
-    my $source = Path::Class::Dir->new($SOURCE);
-    my @files = sort $source->children;
+my $the_questions = get_questions();
 
-    foreach my $pod_file (@files) {
-        next unless $pod_file->stringify =~ /.pod$/;
-        next unless $pod_file->stringify =~ /\d/;
+my $out_handle = Path::Class::Dir->new($SOURCE)->file('perlfaq.pod')->openw();
 
-        # next unless $pod_file->stringify =~ /3/;
+my $tt = Template->new( { POST_CHOMP => 1, } );
+$tt->process( 'perlfaq.tt', { the_questions => $the_questions }, $out_handle )
+    || die $tt->error();
 
-        my $parser = Pod::Simple::XHTML->new;
-        $parser->html_header('');
-        $parser->html_footer('');
-        $parser->html_charset($HTML_CHARSET);
-        my $html = '';
-        $parser->output_string( \$html );
-        $parser->parse_file( $pod_file->stringify );
+$out_handle->close();
 
-        my $root = HTML::TreeBuilder->new_from_content($html);    # empty tree
-        $root->elementify;
+sub get_questions {
 
-        my @doc  = $root->content_list();
-        my @body = $doc[1]->content_list();
+    my $out;
+    {
+        my $source = Path::Class::Dir->new($SOURCE);
+        my @files  = sort $source->children;
 
-        my $html_nodes = scalar(@body);
+        foreach my $pod_file (@files) {
+            next unless $pod_file->stringify =~ /.pod$/;
+            next unless $pod_file->stringify =~ /\d/;
 
-        my %data;
-        my @questions;
+            # next unless $pod_file->stringify =~ /3/;
 
-        for ( my $i = 0; $i <= $html_nodes; $i++ ) {
-            my $h_node = $body[$i] || next;
-            my $tag = $h_node->tag;
-            next unless $tag =~ /^h/;
-            my $text = fetch_text($h_node);
+            my $parser = Pod::Simple::XHTML->new;
+            $parser->html_header('');
+            $parser->html_footer('');
+            $parser->html_charset($HTML_CHARSET);
+            my $html = '';
+            $parser->output_string( \$html );
+            $parser->parse_file( $pod_file->stringify );
 
-            if ( $tag eq 'h2' ) {
+            my $root
+                = HTML::TreeBuilder->new_from_content($html);    # empty tree
+            $root->elementify;
 
-                # All a bit hacky, but works - patches welcome
-                $text =~ s/</E<lt>/g;
-                $text =~ s/([^lt])>/$1E<gt>/g;
-                $text =~ s/CSTART/C</g;
-                $text =~ s/CEND/>/g;
-                push( @questions, $text );
+            my @doc  = $root->content_list();
+            my @body = $doc[1]->content_list();
 
-            } else {
-                $data{$tag}->{$text} = fetch_text( $body[ $i + 1 ] );
+            my $html_nodes = scalar(@body);
+
+            my %data;
+            my @questions;
+
+            for ( my $i = 0; $i <= $html_nodes; $i++ ) {
+                my $h_node = $body[$i] || next;
+                my $tag = $h_node->tag;
+                next unless $tag =~ /^h/;
+                my $text = fetch_text($h_node);
+
+                if ( $tag eq 'h2' ) {
+
+                    # All a bit hacky, but works - patches welcome
+                    $text =~ s/</E<lt>/g;
+                    $text =~ s/([^lt])>/$1E<gt>/g;
+                    $text =~ s/CSTART/C</g;
+                    $text =~ s/CEND/>/g;
+                    push( @questions, $text );
+
+                } else {
+                    $data{$tag}->{$text} = fetch_text( $body[ $i + 1 ] );
+                }
             }
+
+            my $name = $data{'h1'}->{'NAME'};
+            my $desc = $data{'h1'}->{'DESCRIPTION'};
+
+            $name =~ s/(perlfaq.+) -/L<$1>:/;
+
+            $out .= "=head2 $name\n\n";
+            $out .= "$desc\n\n";
+            $out .= "=over 4\n\n";
+
+            foreach my $q (@questions) {
+                $out .= "=item *\n\n";
+                $out .= "$q\n\n";
+            }
+
+            $out .= "=back\n\n\n";
         }
 
-        my $name = $data{'h1'}->{'NAME'};
-        my $desc = $data{'h1'}->{'DESCRIPTION'};
-
-        $name =~ s/(perlfaq.+) -/L<$1>:/;
-
-        print "=head2 $name\n\n";
-        print "$desc\n\n";
-        print "=over 4\n\n";
-
-        foreach my $q (@questions) {
-            print "=item *\n\n";
-            print "$q\n\n";
-        }
-
-        print "=back\n\n\n";
     }
-
+    return $out;
 }
 
 sub fetch_text {
